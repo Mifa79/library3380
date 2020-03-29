ec2ee157f70b36629c3f1b022ce013a5f77391f8
Mifa79 <anh.tx21@gmail.com>
2020-03-29 00:52:53 -0500

    Create app home_page, add url in urls.py, create html file for homepage

---
+++ src/home_page/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns=[
+    path('',views.home,name='home'),
+    # path('login',views.login,name='login'),
+    # # path('',views.account,name='account'),
+    # path('accountPage',views.accountPage,name='account page')
+]
