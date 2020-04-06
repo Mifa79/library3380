77c2971013c2e216599d501640decf5cabe53dce
Mifa79 <anh.tx21@gmail.com>
2020-04-05 23:16:53 -0500

    Delete console log and comment code

--- src/library3380/authentication.py
+++ src/library3380/authentication.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth.hashers import check_password
 # from django.contrib.auth.models import User
-# from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.db import connection
 
--- src/login/views.py
+++ src/login/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 # from django.contrib.auth.models import User, auth
 from django.contrib.auth.models import Group
-# from .models import Link
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
@@ -19,16 +18,13 @@ def myLogin(request):
 
             # the "aunthenticate" function was written using python and SQL in "authentication.py" in "library3380" folder 
             user = authenticate(request, username=username, password=password)
-            # userGroup = user.groups.all()[0]
 
             if user is not None:
                 cursor.execute("SELECT user_type FROM sign_up_user WHERE username= %s", [username])
                 user_type = [item[0] for item in cursor.fetchall()]
-                print(user_type)
 
                 if(user_type[0] == "1" or user_type[0] == "2"):
                     login(request, user)
-                    # print (request.user.is_authenticated)
                     return redirect('/my_login/accountPage')
                 else:
                     login(request, user)
@@ -46,15 +42,6 @@ def myLogin(request):
 @login_required(login_url='/my_login')
 def accountPage(request):
     user = request.user
-#     globalUrls = Link.objects.filter(links__name='Global')  # returns queryset using foreign key to where group is Global
-#     userGroup = user.groups.all()[0]  # object
-
-#     if (userGroup.name != "Global"):
-#         userUrls = Link.objects.filter(links__name=userGroup.name)  # queryset
-#         links = list(chain(globalUrls, userUrls))  # concat querysets
-#         context = {'links': links, 'Role': userGroup.name}
-#     else:
-#         context = {'links': globalUrls,'Role':userGroup.name}
     context = {'user': user}
     return render(request, 'index.html', context)
 
