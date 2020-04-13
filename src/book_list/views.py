1d03e2a839a6bffda635f89f4a098d72e3caad4d
Mifa79 <anh.tx21@gmail.com>
2020-04-13 17:16:03 -0500

    Fetch book data from database to display book details dynamically

--- src/book_list/views.py
+++ src/book_list/views.py
@@ -24,13 +24,26 @@ def book_list_by_category(request, category):
         # get all the books in a particular subject
         cursor.execute("SELECT ISBN, book_title, book_author FROM book WHERE book_subject= %s", [category])
         books = dictfetchall(cursor)
+        # print("books are: ", books)
+        # print(books.book.book_title)
         context = {'subjects': subjects, 'books': books, "category": category}
     return render(request, 'book_list.html', context)
 
 
 def book_details_page(request, ISBN):
     print("Book ISBN is ", ISBN)
-    return render(request, 'book_details.html')
+    with connection.cursor() as cursor:
+        # get all the details of the book
+        cursor.execute("SELECT ISBN, book_title, book_author, book_subject, book_publisher, date_of_publication, MSRP FROM book WHERE ISBN= %s", [ISBN])
+        # cursor.execute("SELECT ISBN, book_title, book_author FROM book WHERE book_subject= %s", [category])
+
+        book_details = dictfetchall(cursor)
+        book_detail = book_details[0]
+        # book_detail = cursor.fetchall()
+        print("Book Details is: ", book_detail)
+        # print(book_detail.book_title)
+        context = {'book_detail': book_detail}
+    return render(request, 'book_details.html', context)
 
 
 def dictfetchall(cursor):
