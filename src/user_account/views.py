a1b34a21159f2e4d563b6d1423869212f1c88b3b
Mifa79 <anh.tx21@gmail.com>
2020-04-14 00:05:50 -0500

    Fetch user data from database to user_account page

--- src/user_account/views.py
+++ src/user_account/views.py
@@ -1,15 +1,41 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as myLogout
 from django.contrib.auth.decorators import login_required
+from django.db import connection
+from datetime import date
+from datetime import datetime, timedelta
+from django.contrib import messages
 
 # Create your views here.
 @login_required(login_url='/my_login')
 def userAccount(request):
-    user = request.user
-    context = {'user': user}
-    return render(request, 'user_account.html', context)
+    with connection.cursor() as cursor:
+        user = request.user
+        print("user is: ", user)
+
+        # get user info from the database
+        cursor.execute("SELECT id, user_type FROM sign_up_user WHERE username= %s", [user])
+        user_info = dictfetchall(cursor)
+        user_info = user_info[0]
+        print ("user info is: ", user_info)
+        print("user_ID is: ", user_info['id'])
+        user_ID = user_info['id']
+        print("user type is: ", user_info['user_type'])
+        user_type = user_info['user_type']
+
+        context = {'user': user}
+        return render(request, 'user_account.html', context)
 
 @login_required
 def logout(request):
     myLogout(request)
     return redirect('/')
+
+
+
+def dictfetchall(cursor):
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
